dea83c1c6c6fe5080b66e63349d42feb0f5f8d71
System Administrator <james@jadaradix.com>
2015-02-01 13:24:57 +0000

    Fix hash links (#tech) from external pages.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 $(window).load(function() {
 
-  $("[data-scroll-to]").click(function(jEvent) {
-    var el = $("#" + $(jEvent.target).attr("data-scroll-to"));
+  function doScrollTo(what) {
+    var el = document.getElementById(what);
+    if (!el) return;
+    el = $(el);
     async.waterfall([
       function(next) {
         $(".section-wrapper.hide").css("display", "none");
         el.css("display", "block");
-        // el.animate(
-        //   { height: "100%" },
-        //   1000,
-        //   "swing"
-        // );
-        // el.show(250, next);
         next();
       },
       function(next) {
@@ -24,7 +20,17 @@ $(window).load(function() {
         );
       }
     ]);
+  }
+
+  $("[data-scroll-to]").click(function(jEvent) {
+    doScrollTo($(jEvent.target).attr("data-scroll-to"));
     return false;
   });
+
+  var windowHash = window.location.hash;
+  if (windowHash.length >= 2) {
+    windowHash = windowHash.substring(1);
+    doScrollTo(windowHash);
+  }
 
 });
