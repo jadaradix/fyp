a372a75505fb88cf86d9f58a57e7b094954160d3
James Garner <james@jadaradix.com>
2015-02-03 17:08:29 +0000

    Colour hack to show the right shade of red when it goes wrong.

--- js/process.js
+++ js/process.js
@@ -20,83 +20,107 @@
       }
     }
 
+    //color hack
+    var colors = $("#processing > span");
+    var colorGreen = colors.css("color");
+    var colorRed = colors.css("background-color");
+
+    var drawTimeoutId;
+    var stopDrawing;
+    var pollTimeout = 5000;
     var pollTimeoutId;
+    var polledTimes = 0;
+    var polledTimesLimit = 4;
+    function pollCleanUp() {
+      clearInterval(pollTimeoutId);
+      clearInterval(metricSwitchId);
+    }
     function poll() {
       easyAjax("api/process/" + twitterAccountName, function(data) {
+
         if ("error" in data) {
-          clearInterval(metricSwitchId);
-          clearInterval(pollTimeoutId);
+          pollCleanUp();
           window.location = "../twitter/" + twitterAccountName;
           return;
         }
-        if (!gData) {
-          gData = data;
-          metricSwitchId = setInterval(metricSwitch, 7500);
-          setTimeout(metricSwitch, 0);
 
-          async.waterfall([
-            function(next) {
-              //matrix setup
-              function resize() {
-                keepSizeRatio(label, (480 / 640) * 0.15);
-                keepSizeRatio(canvas, (480 / 640));
-                label.css("top", ((canvas.height() / 2) - (label.height() / 2)) + "px");
-              }
-              $(window).resize(resize);
-              setTimeout(resize, 0); //don't ask
-              label.css("display", "block");
-              canvas.css("display", "block");
-              next();
-            },
-            function(next) {
-              //matrix drawing
-              var width = canvas.attr("width");
-              var height = canvas.attr("height");
-              var ctx = canvas[0].getContext("2d");
-              var bgColor = "black";
-              var fgColor = canvas.css("color");
-              var timesPerSecond = 8;
-              var lineCount = height / 16;
-              var allLines = $.map(gData.twitter.tweets, function(tweet) {
-                var nLine = "> " + tweet.text;
-                // var rr1 = _.random(width / 16 / 4);
-                // var rr2 = _.random(width / 16);
-                // nLine = nLine.substring(width / 16 / 4);
-                // nLine = nLine.substring(0, nLine.length - (width / 16 / 4));
-                // _(rr2).times(
-                //   function(n) { nLine = " " + nLine; }
-                // );
-                return nLine;
-              });
-              lines = [];
-              _(lineCount).times(
-                function(n) { lines.push(allLines[_.random(allLines.length)]); }
-              );
-              var draw = function() {
-                ctx.fillStyle = bgColor;
-                ctx.fillRect(0, 0, width, height);
-                ctx.fillStyle = fgColor;
-                ctx.font = "normal 16px 'Courier New'";
-                $.each(lines, function(index, line) {
-                  ctx.fillText(line, 0, (index + 1) * 16);
-                });
-                lines.shift(); //remove first element
-                lines.push(allLines[_.random(allLines.length)]);
-              };
-              setInterval(draw, 1000 / timesPerSecond);
-            }
-          ]);
+        polledTimes++;
 
+        console.log("poll %d", polledTimes);
+        if (polledTimes >= polledTimesLimit) {
+          pollCleanUp();
+          $("#processing > div p span").html("> Damn.");
+          stopDrawing = true;
+          console.log("I have received results from %d polls now.", polledTimesLimit);
         }
+
+        //data already present - we're done
+        if (gData) return;
+
+        gData = data;
+        metricSwitchId = setInterval(metricSwitch, 7500);
+        setTimeout(metricSwitch, 0);
+
+        async.waterfall([
+          function(next) {
+            //matrix setup
+            function resize() {
+              keepSizeRatio(label, (480 / 640) * 0.15);
+              keepSizeRatio(canvas, (480 / 640));
+              label.css("top", ((canvas.height() / 2) - (label.height() / 2)) + "px");
+            }
+            $(window).resize(resize);
+            setTimeout(resize, 0); //don't ask
+            label.css("display", "block");
+            canvas.css("display", "block");
+            next();
+          },
+          function(next) {
+            //matrix drawing
+            var width = canvas.attr("width");
+            var height = canvas.attr("height");
+            var ctx = canvas[0].getContext("2d");
+            var timesPerSecond = 8;
+            var lineCount = height / 16;
+            var allLines = $.map(gData.twitter.tweets, function(tweet) {
+              var nLine = "> " + tweet.text;
+              return nLine;
+            });
+            lines = [];
+            _(lineCount).times(
+              function(n) { lines.push(allLines[_.random(allLines.length - 1)]); }
+            );
+            ctx.font = "normal 16px 'Courier New'";
+            var draw = function() {
+              ctx.fillStyle = "black";
+              ctx.fillRect(0, 0, width, height);
+              ctx.fillStyle = colorGreen;
+              if (stopDrawing) {
+                $("#processing > div p")
+                  .css("color", colorRed)
+                  .css("border-color", colorRed);
+                ctx.fillStyle = colorRed;
+              }
+              $.each(lines, function(index, line) {
+                ctx.fillText(line, 0, (index + 1) * 16);
+              });
+              lines.shift(); //remove first element
+              lines.push(allLines[_.random(allLines.length - 1)]);
+              if (stopDrawing) clearInterval(drawTimeoutId);
+            };
+            drawTimeoutId = setInterval(draw, 1000 / timesPerSecond);
+          }
+        ]);
+
         if (gData.topics.length >= 1) {
-          clearInterval(metricSwitchId);
-          clearInterval(pollTimeoutId);
+          pollCleanUp();
           window.location = "../museum/" + twitterAccountName;
           return;
         }
+
       });
     }
-    pollTimeoutId = setInterval(poll, 5000);
+    pollTimeoutId = setInterval(poll, pollTimeout);
     setTimeout(poll, 0);
 
   });
