9fd3b21614d853a643f64ab145c09f59485566b1
System Administrator <james@jadaradix.com>
2015-02-05 14:29:37 +0000

    Share on Twitter functionality.

--- js/museum.js
+++ js/museum.js
@@ -2,6 +2,9 @@
 
   $(document).ready(function() {
 
+    var shareBox = $(document.getElementById("share-box"));
+    shareBox.val(window.location.href);
+
     // var test = $("#test");
     // console.log(test);
     // test.bind("load", function(e) {
